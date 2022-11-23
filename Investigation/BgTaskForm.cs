60c403879c912229414fcbe3b9f63a1991825715
Takayoshi Nakagawa <takayoshi@tk-nkgw.jp>
2022-11-23 14:23:27 +0900

    初回追加

---
+++ Investigation/BgTaskForm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Investigation
+{
+    public partial class BgTaskForm : Form
+    {
+
+        private Timer displayTimer = new Timer();
+
+        public BgTaskForm()
+        {
+            InitializeComponent();
+        }
+
+        Action<object> threadFunc = null;
+
+        public BgTaskForm(Action<object> inThreadFunc)
+        {
+            InitializeComponent();
+            threadFunc = inThreadFunc;
+        }
+
+        private void BgTaskForm_Load(object sender, EventArgs e)
+        {
+            displayTimer.Tick += TimerElapsed;
+            displayTimer.Interval = 2000;
+            displayTimer.Enabled = true;
+            var operationThread = new System.Threading.Thread(new System.Threading.ParameterizedThreadStart(threadFunc));
+            operationThread.Start(this);
+        }
+
+        public DialogResult showDialog()
+        {
+            if (threadFunc != null)
+            {
+                this.Opacity = 0;
+            }
+
+            return ShowDialog();
+        }
+
+        private void TimerElapsed(object sender, EventArgs e)
+        {
+            displayTimer.Enabled = false;
+            if (InvokeRequired)
+            {
+                Invoke((MethodInvoker)(() => { Opacity = 100; }));
+            }
+            else
+            {
+                this.Opacity = 1;
+            }
+        }
+    }
+}
---
+++ Investigation/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Investigation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            BgTaskForm form = new BgTaskForm(threadFunc);
+            form.showDialog();
+        }
+
+        public void threadFunc(object inObject)
+        {
+            Form inForm = (Form)inObject;
+            System.Threading.Thread t = new System.Threading.Thread(
+                                        new System.Threading.ThreadStart((MethodInvoker)(() =>
+                                        {
+                                            System.Threading.Thread.Sleep(1000);
+                                            Console.WriteLine("Complete2" + Environment.NewLine);
+                                        })));
+            t.Start();
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine("Complete1" + Environment.NewLine);
+            t.Join();
+            inForm.Invoke((MethodInvoker)(() => { inForm.Close(); }));
+        }
+
+    }
+}
